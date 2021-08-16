2a153db581c4393260365498897eea1cd514201f
ANTONYTHIVAKAR <antonythivakar@gmail.com>
2021-08-16 17:51:43 +0530

    form submit action via api

--- js/formValidation.js
+++ js/formValidation.js
@@ -51,11 +51,32 @@ if (form != null) {
 		var errorContainer = document.getElementById("FormSubmitError");
 		errorContainer.innerHTML = "";
 		e.preventDefault();
-		for (i = 0; i < form.length; i++) {
-				if (form[i].nodeName === "INPUT") {
-					console.log(form[i].value.toLocaleUpperCase());
-				}
-			}
+		var form = document.getElementById("contactForm");
+		if(form == undefined || form  == null) {
+			form = document.getElementById("serviceForm");
+		}
+  async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+	console.log(new FormData(event.target), 'schema');
+	console.log(event, 'schema event');
+
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      status.innerHTML = "Thanks for your submission! We will get back to you";
+      form.reset()
+    }).catch(error => {
+      status.innerHTML = "Oops! There was a problem submitting your form"
+    });
+  }
+  form.addEventListener("submit", handleSubmit)
+
 });
 }
 
